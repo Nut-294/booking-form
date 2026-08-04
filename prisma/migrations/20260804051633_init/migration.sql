3c2a67ff6a11c8086c4edb12f6bddc61748b541e
nutnut <sonchainut7752@gmail.com>
2026-08-04 12:18:55 +0700

    Install Prisma

---
+++ prisma/migrations/20260804051633_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Booking" (
+    "id" TEXT NOT NULL,
+    "checkIn" TIMESTAMP(3) NOT NULL,
+    "checkOut" TIMESTAMP(3) NOT NULL,
+    "guests" INTEGER NOT NULL,
+    "rooms" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
+);
