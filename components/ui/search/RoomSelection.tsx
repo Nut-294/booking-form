139dfe369c88f49b4172fd9727d611d8be8c4648
nutnut <sonchainut7752@gmail.com>
2026-09-08 22:56:24 +0700

    RoomSelection

--- components/ui/search/RoomSelection.tsx
+++ components/ui/search/RoomSelection.tsx
@@ -18,17 +18,34 @@ function RoomSelection({ rooms, params }: RoomSelectionProp) {
 
   const handleSelect = (id: string) => {
     setSelectRooms((prev) => {
+      // ถ้าห้องนี้ถูกเลือกอยู่แล้ว → ยกเลิกการเลือก
       if (prev.includes(id)) {
-        return prev.filter((roomId) => roomId != id);
+        return prev.filter((roomId) => roomId !== id);
+      }
+      // ถ้าเลือกห้องครบตามจำนวนที่ลูกค้าต้องการแล้ว
+      if (prev.length >= params.rooms) {
+        return prev;
       }
       return [...prev, id];
     });
   };
   console.log("select", selectedRooms);
 
+  //คำนวณจำนวนที่ห้องพักรองรับได้ -> จาก id ห้องที่เลือกแล้ว (selectedRooms)
+  const totalRoomCapacity = selectedRooms.reduce((total, roomId) => {
+    //เอา id ห้องที่เลือกแต่ละห้อง ไปหาข้อมูลห้อง
+    const room = rooms.find((room) => room.id === roomId);
+    //เอา capacity ของ room มาบวก
+    const totalCapacity = total + (room?.roomType.capacity ?? 0);
+    return totalCapacity;
+  }, 0);
+
   return (
     <div>
-      <h5 className="text-2xl my-4">RoomSelect : {selectedRooms.length}</h5>
+      <div className="flex justify-between items-center mx-20">
+        <h5 className="text-2xl my-4">RoomSelect : {selectedRooms.length}</h5>
+        <h5 className="text-2xl my-4">RoomCapacity : {totalRoomCapacity}</h5>
+      </div>
       <div className="grid grid-cols-3 gap-4">
         {rooms.map((room) => {
           return (
